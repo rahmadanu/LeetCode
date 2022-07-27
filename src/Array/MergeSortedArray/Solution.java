c9438188e8688707c80183d736c8c50666ef651c
rahmadanu <sadewarahmadanu@gmail.com>
2022-07-28 00:39:30 +0700

    Merging from RTL or LTR

--- src/Array/MergeSortedArray/Solution.java
+++ src/Array/MergeSortedArray/Solution.java
@@ -1,31 +1,55 @@
 package Array.MergeSortedArray;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
         int i = 0, j = 0, k = 0;
+        int[] nums3 = new int[m + n];
 
-        int[] nums3 = new int[50];
-
-        while (i < nums1.length && j < nums2.length) {
+        //Forward
+        while (i < m && j < n) {
 
             if (nums1[i] < nums2[j]) {
                 nums3[k++] = nums1[i++];
-            } else if (nums1[i] > nums2[j]) {
+            } else {
                 nums3[k++] = nums2[j++];
+            }
+            System.out.println(Arrays.toString(nums3));
+        }
+
+        while (i < m) {
+            nums3[k++] = nums1[i++];
+            System.out.println(Arrays.toString(nums3));
+        }
+
+        while (j < n) {
+            nums3[k++] = nums2[j++];
+            System.out.println(Arrays.toString(nums3));
+        }
+
+        /* Backward
+        int i = m-1, j = n-1, k = m + n -1;
+        int[] nums3 = new int[m + n];
+
+        while (i >= 0 && j >= 0) {
+
+            if (nums1[i] > nums2[j]) {
+                nums3[k--] = nums1[i--];
+            } else {
+                nums3[k--] = nums2[j--];
             }
         }
 
-        while (i < nums1.length) {
-            nums3[k++] = nums1[i++];
+        while (i >= 0) {
+            nums3[k--] = nums1[i--];
         }
-
-        while (j < nums2.length) {
-            nums3[k++] = nums2[j++];
+        while (j >= 0) {
+            nums3[k--] = nums2[j--];
         }
-
+*/
         System.out.println(Arrays.toString(nums3));
     }
 
