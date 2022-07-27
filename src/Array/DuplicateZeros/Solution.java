686d8ad7b1bf08374c98a9375802fc178fbfde92
rahmadanu <sadewarahmadanu@gmail.com>
2022-07-27 17:41:32 +0700

    Adding brute force approach

--- src/Array/DuplicateZeros/Solution.java
+++ src/Array/DuplicateZeros/Solution.java
@@ -3,9 +3,28 @@ package Array.DuplicateZeros;
 import java.util.Arrays;
 
 class Solution {
-    public void duplicateZeros(int[] arr) {
+    public void duplicateZeros(int[] arr) { // will use queue later
         System.out.println(Arrays.toString(arr));
 
+        if (arr == null || arr.length == 0) return;
+
+/*      Brute force approach
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("i in first loop: " + i);
+            if (arr[i] == 0) {
+
+                for (int j = arr.length - 1; j > i; j--) {
+                    arr[j] = arr[j-1];
+                    System.out.println(Arrays.toString(arr));
+                }
+                i++;
+                System.out.println("i in if: " + i);
+            }
+        }
+*/
+
+        //Optimized solution
         int i = 0, shift = 0;
 
         for (i = 0; shift + i < arr.length; ++i) {
@@ -30,7 +49,9 @@ class Solution {
         }
 
         System.out.println(Arrays.toString(arr));
-    } // will use queue later
+
+    }
+
 
     public static void main(String[] args) {
         Solution obj = new Solution();
