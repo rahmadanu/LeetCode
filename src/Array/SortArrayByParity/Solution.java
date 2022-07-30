433d06fde86c9412f0c636c9a6f759d4cb9018b2
rahmadanu <sadewarahmadanu@gmail.com>
2022-07-31 03:14:53 +0700

    Adding additional notes

--- src/Array/SortArrayByParity/Solution.java
+++ src/Array/SortArrayByParity/Solution.java
@@ -5,15 +5,18 @@ import java.util.Arrays;
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
 
+        if (nums == null && nums.length == 0) return null;
+
         int k = 0;
 
         for (int i = 0; i < nums.length; i++) {
 
             if (nums[i] % 2 == 0) {
+                System.out.println(Arrays.toString(nums));
                 int temp = nums[i];
                 nums[i] = nums[k];
                 nums[k++] = temp;
-            }
+            }  //input containing [2, 2, 2] will run in loop which is unnecessary to swap, will fix later
         }
 
         return nums;
@@ -21,6 +24,6 @@ class Solution {
 
     public static void main(String[] args) {
         Solution obj = new Solution();
-        System.out.println(Arrays.toString(obj.sortArrayByParity(new int[] {3,1,2,4})));
+        System.out.println(Arrays.toString(obj.sortArrayByParity(new int[] {2, 3, 2})));
     }
 }
