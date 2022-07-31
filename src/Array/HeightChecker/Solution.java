0954312f0ad5831ace46cab42272ca55fcac9e2a
rahmadanu <sadewarahmadanu@gmail.com>
2022-07-31 14:05:49 +0700

    Brute force approach O(n log n) because of .sort() function

---
+++ src/Array/HeightChecker/Solution.java
@@ -0,0 +1,34 @@
+package Array.HeightChecker;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+class Solution {
+    public int heightChecker(int[] heights) {
+
+        if (heights == null || heights.length == 0) return 0;
+
+        int count = 0;
+
+        int[] expected = new int[heights.length];
+
+        for (int i = 0; i < heights.length; i++) {
+
+            expected[i] = heights[i];
+        }
+
+        Arrays.sort(expected);
+
+        for (int i = 0; i < heights.length; i++){
+
+            if (heights[i] != expected[i]) count++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        System.out.println(obj.heightChecker(new int[] {1,1,4,2,1,3}));
+    }
+}
