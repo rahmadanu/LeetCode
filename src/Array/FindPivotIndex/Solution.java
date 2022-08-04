66e0433cd949857ab588c43862541721482c98f3
rahmadanu <sadewarahmadanu@gmail.com>
2022-08-04 17:02:31 +0700

    Initial commit

--- src/Array/FindPivotIndex/Solution.java
+++ src/Array/FindPivotIndex/Solution.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 class Solution {
     public int pivotIndex(int[] nums) { // says input [1, 2, 1]
-
         int[] leftSum = new int[nums.length];
         int[] rightSum = new int[nums.length];
         leftSum[0] = nums[0];
@@ -13,7 +12,7 @@ class Solution {
         for (int i = 1; i < nums.length; i++) {
 
             leftSum[i] = nums[i] + leftSum[i-1]; // [1, 3, 4]
-            rightSum[nums.length - 1 - i] = nums[nums.length - 1 - i] + rightSum[nums.length-1]; // [4, 3, 1]
+            rightSum[nums.length - 1 - i] = nums[nums.length - 1 - i] + rightSum[nums.length-i]; // [4, 3, 1]
         }
 
         System.out.println(Arrays.toString(leftSum));
