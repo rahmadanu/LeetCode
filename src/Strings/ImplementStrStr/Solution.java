216bc6874ed8d8793979254bce2997a50e339465
rahmadanu <sadewarahmadanu@gmail.com>
2022-08-13 13:52:43 +0700

    Optimized solution

---
+++ src/Strings/ImplementStrStr/Solution.java
@@ -0,0 +1,13 @@
+package Strings.ImplementStrStr;
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+
+        return haystack.indexOf(needle);
+    }
+
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        System.out.println(obj.strStr("danu", "d"));
+    }
+}
